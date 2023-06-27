267ea273e802b7b9a5a38fc39dcf4bb86135405f
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-27 17:19:14 +0300

    Add components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+import { dltUser } from "../../redux/operations.js";
+import {
+  getTasks,
+  getStatusFilter,
+  errorMessage,
+  getLoading
+} from "../../redux/contacts/selectors.js";
+const TaskList = styled.ul`
+  display: block;
+  width: 600px;
+  list-style: none;
+  list-style-type: none;
+`;
+const List = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  list-style: none;
+  list-style-type: none;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+const Button = styled.button`
+  border: 1px solid;
+  margin-right: 4px;
+  margin-left: 4px;
+`;
+export default function ContactList() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getTasks);
+  const isLoading = useSelector(getLoading);
+  const error = useSelector(errorMessage);
+  const filtered = useSelector(getStatusFilter);
+  const contactsName = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filtered.toLowerCase())
+  );
+  return (
+    <TaskList>
+      {error || <p>Add contact please!</p>}
+      <li>
+        <h2>Contacts</h2>
+        {!contacts.length < 0 ? <p>No data contacts!</p> : undefined}
+      </li>
+      {contactsName?.length || isLoading ? (
+        [...contactsName].map((contact, idx, arr) => (
+          <List key={contact.id}>
+            {contact.name + ":" + contact.phone}
+            <Button
+              type="button"
+              name="delete"
+              onClick={() => dispatch(dltUser(contact.id))}
+            >
+              Delete
+            </Button>
+          </List>
+        ))
+      ) : (
+        <p>Not available contacts!</p>
+      )}
+    </TaskList>
+  );
+}
