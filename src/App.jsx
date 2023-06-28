0d9413695ec918592f7019db1e48d3830f124997
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-28 10:57:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, Routes} from 'react-router-dom';
 import {Suspense} from 'react';
 import { useEffect } from 'react';
-import Layout from './components/Layout.jsx'
-import Collection from './components/Collection/Collection.jsx';
-import Register from './components/Register/Register.jsx';
+import {Layout} from './components/Layout.jsx'
+import {Collection} from './components/Collection/Collection.jsx';
+import {Register} from './components/Register/Register.jsx';
 import {useDispatch} from 'react-redux';
 import {getAllcontacts} from './redux/operations.js';
 
