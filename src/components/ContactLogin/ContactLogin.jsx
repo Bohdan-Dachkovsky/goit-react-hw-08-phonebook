0238440683ba84af9892e576742261ce57776008
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-28 15:02:33 +0300

    Update

--- src/components/ContactLogin/ContactLogin.jsx
+++ src/components/ContactLogin/ContactLogin.jsx
@@ -1,8 +1,31 @@
 import { Link, Outlet } from "react-router-dom"
-
+import formCss from './form.module.css'
 const ContactLogin = () => {
 return (
 <div>
+<form className={formCss.wrapperForm}>
+      <label>
+        Name&nbsp;
+        <input
+          type="text"
+          name="email"
+          className={formCss.field}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <label>
+        Email &nbsp;
+        <input
+          type="text"
+          name="email"
+          className={formCss.field}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <button type="submit" className={formCss.clickBtn}>Enter</button>
+    </form>
 <Link to ='/contacts'>Phonebook acc</Link>
 <Outlet/>
 </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,41 @@
+import style from './formStyle.module.css'
 const Register = () => {
 return (
-    <></>
+    <div>
+<form className={style.wrapperForm}>
+      <label className={style.wrapperForm}>
+        Name&nbsp;
+        <input
+          type="text"
+          name="email"
+          className={style.field}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <label className={style.wrapperForm}>
+        Email &nbsp;
+        <input
+          type="text"
+          name="email"
+          className={style.field}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <label className={style.wrapperForm}>
+        Password &nbsp;
+        <input
+          type="text"
+          name="password"
+          className={style.field}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <button type="Submit" className={style.clickBtn}>Register</button>
+      </form>
+    </div>
     )
 }
 export default Register
