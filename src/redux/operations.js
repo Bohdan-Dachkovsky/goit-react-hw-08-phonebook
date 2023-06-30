2ad7bad5ab43c907310dd267e527f4899b5df530
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-30 15:41:03 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -12,7 +12,7 @@ axios.defaults.headers.common.Authorization = ''},
 
 export const register = createAsyncThunk('task/register', async (credentials, thunkAPI) => {
       try {
-            const response = await axios.get(`/users/signup`, credentials)
+            const response = await axios.post(`/users/signup`, credentials)
             // token.set(data.token)
             console.log(response.data)
             return response.data
