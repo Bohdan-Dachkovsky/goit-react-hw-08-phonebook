217c1224d31b3f19129b92a8ffb984cac05a6956
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-06-30 16:58:28 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/ ';
+axios.defaults.baseURL = 'https://goit-task-manager.herokuapp.com/';
 
 const token = {
 set(token) {
