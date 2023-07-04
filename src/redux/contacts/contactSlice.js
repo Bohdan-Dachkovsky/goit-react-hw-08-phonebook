b9f36becd45c73daf5c6246f8f546e33f8a502d2
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-07-04 13:43:28 +0300

    Update contactSlice.js

--- src/redux/contacts/contactSlice.js
+++ src/redux/contacts/contactSlice.js
@@ -13,9 +13,9 @@ export const contactSlice = createSlice({
   name: "users",
   initialState: {
     items: [   
-    {id: '1', number: '093-8383-355'},
-    {id: '2', name: 'Jonsion Resta', number: '093-8459-689'},
-    {id: '3', name: 'Boris Bankli', number: '095-4543-344'},],
+    {id: 1, number: '093-8383-355'},
+    {id: 2, name: 'Jonsion Resta', number: '093-8459-689'},
+    {id: 3, name: 'Boris Bankli', number: '095-4543-344'},],
     isLoading: false,
     error: null,
     filter: "",
